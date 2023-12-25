8d58fa93ca46b94010dcfed4885aeaae4e2ce34e
linuxgamingcentral <linuxgamingcentral@pm.me>
2023-12-25 06:38:09 -0500

    add support for Project+ and PMEX Remix

--- README.md
+++ README.md
@@ -3,35 +3,41 @@ The goal of this project is to be the ultimate Swiss army knife for all things *
 
 By the time the project is complete I'm hoping to be able to do the following:
 - seamlessly download, update, play, and configure:
-  - Smash 64 Remix
-  - Slippi
-  - Project+
-  - Project M EX Remix
-  - Smash Ultimate HDR
+  - [Smash 64 Remix](https://github.com/JSsixtyfour/smashremix)
+  - [Slippi](https://github.com/project-slippi/slippi-launcher)
+  - [Project+](https://projectplusgame.com/)
+  - [Project M EX Remix](https://linuxgamingcentral.com/posts/how-to-setup-project-m-ex-on-deck/)
+  - [Smash Ultimate HDR](https://github.com/HDR-Development/HDR-Releases)
 - download and apply mods
 - overclock the GCC adapter
 
 ## What Currently Works
 - Smash 64 Remix
+  - can't launch the ROM directly through the launcher; you'll just need to add it via Steam ROM Manager
 - Slippi
+  - manual patching of Animelee and Diet Melee
+- Project+
+  - automatic configuration of SD card path, default ISO, and launcher directory
+- PMEX Remix
+  - currently requires you to manually download the mod and manually setting your paths (textures are automatically copied though)
 - overclocking the GCC adapter
+  - if the overclock module already exists you have the option to uninstall
 
 ## To-Do
 - Add support for:
-  - Project+
-  - PMEX Remix
   - HDR
 - add the ability to seamlessly patch Melee with Animelee and other mods
 - allow users to add games as non-Steam shortcuts
+- netplay with Ryujinx?
 
 ## How to Use
-Be aware this repository does NOT contain any copyrighted assets. You must provide your own legally-dumped copies of your ROMs/ISOs.
+This repository does NOT contain any copyrighted assets. You must provide your own legally-dumped copies of your ROMs/ISOs.
 
 Please run [EmuDeck](https://www.emudeck.com/) first to get the necessary emulators installed. Emulators you'll want to install, if they're not installed already, include:
 - Mupen64 (N64)
 - Ryujinx (Switch)
 
-Slippi, Project+, and PMEX Remix already come with their own AppImage.
+Slippi and Project+/PMEX Remix already come with their own AppImage.
 
 If you're on Steam Deck, download the [desktop file](https://raw.githubusercontent.com/linuxgamingcentral/competitive-smash-decker/main/competitive-smash-decker.desktop) (right-click, save link as) and save it to your desktop. Double-click or tap the file to download and run the script.
 
@@ -41,10 +47,15 @@ Other distros can run the script with:
 
 The script will look for ROMs/ISOs in `~/Emulation/roms/`:
 - Smash 64: `n64/smash64.z64`
-- Melee: `gamecube/ssbm.iso`
+- Slippi: `gamecube/ssbm.iso`
 - Project+/PMEX Remix: `wii/ssbb.iso`
 - HDR: `switch/ssbu.nsp`
 
 If the script can't detect these ROMs, certain functions in the script won't work.
 
-After your ROMs have been patched, they'll show up in `~/Emulation/roms/<system>/`. From here you can run Steam ROM Manager with EmuDeck to add your mods as non-Steam shortcuts.
+After your ROMs have been patched, they'll show up in `~/Emulation/roms/<system>/`. From here you can run Steam ROM Manager with EmuDeck to add your mods as non-Steam shortcuts, launch them directly through the script, or manually add them to Steam.
+
+## Things to be Aware of
+While this script tries to make things as simple as possible for the end-user, there are some parts of the script where there will need to be manual intervention. As an example, some mods are uploaded in some sketchy places (Mediafire, Google Drive, etc.). As a result, it makes downloading these mods through the terminal a bit of a pain. Patching your Melee ISO with Animelee or Diet Melee will currently require you to manually download the mod and apply the patch yourself. Downloading PMEX Remix will require you to open up the download link in your web browser. If I get the developers' permission I might just upload these mods to my own server to make downloading/patching mods a bit easier.
+
+Another thing to be mindful of is, since PMEX Remix uses Project+ as the base, you'll need to keep changing your SD card path if you switch between Project+ and PMEX Remix.
--- competitive-smash-decker.sh
+++ competitive-smash-decker.sh
@@ -24,7 +24,7 @@ info() {
 
 progress_bar() {
 	t=$1
-	zen_nospam --title "$title" --text "$1" --progress --pulsate --auto-close --auto-kill --width=300 --height=100
+	zen_nospam --title "$title" --text "$1" --progress --auto-close --auto-kill --width=300 --height=100
 
 	if [ "$?" != 0 ]; then
 		echo -e "\nUser canceled.\n"
@@ -75,7 +75,7 @@ slippi_menu() {
 	--column="Description"\
 	FALSE Launcher "Download or update the Slippi Launcher"\
 	FALSE Launch_Launcher "Launch Slippi Launcher"\
-	FALSE Slippi "Launch and/or configure Slippi (without launcher)"\
+	FALSE Slippi "Configure Slippi (without launcher)"\
 	FALSE Mods "Get mods"\
 	TRUE Exit "Exit this submenu"
 }
@@ -91,6 +91,33 @@ mods_menu() {
 	TRUE Exit "Exit this submenu"
 }
 
+projectplus_menu() {
+	zen_nospam --width 700 --height 350 --list --radiolist --multiple --title "$title"\
+	--column ""\
+	--column "Option"\
+	--column="Description"\
+	FALSE Download "Download or update Project+"\
+	FALSE Configure "Configure Project+"\
+	FALSE SD "Automatically add SD card path, default ISO, and launcher directories (need to run P+ first)"\
+	FALSE Netplay "Launch Project+ with netplay"\
+	FALSE Offline "Launch Project+ without netplay"\
+	False Changelog "View changelog (will open your web browser)"\
+	TRUE Exit "Exit this submenu"
+}
+
+pmex_menu() {
+	zen_nospam --width 700 --height 350 --list --radiolist --multiple --title "$title"\
+	--column ""\
+	--column "Option"\
+	--column="Description"\
+	FALSE Download "Download or update PMEX Remix"\
+	FALSE Configure "Configure P+"\
+	FALSE Netplay "Launch PMEX Remix with netplay"\
+	FALSE Offline "Launch PMEX Remix without netplay"\
+	False Guide "View my guide (in case you get stuck)"\
+	TRUE Exit "Exit this submenu"
+}
+
 # functions
 get_sudo_password() {
 	sudo_password=$(zen_nospam --password --title="$title")
@@ -103,32 +130,6 @@ get_sudo_password() {
 		echo -e "Password entered, let's proceed...\n"
 	fi
 }
-
-patch_iso() {
-	# this is just temporary code for now and isn't currently used in the script
-	if ! [ -f $HOME/Emulation/roms/gamecube/ssbm.iso ]; then
-		error "SSBM ISO not found. Please place it in $HOME/Emulation/roms/gamecube/ and name it to ssbm.iso"
-	else
-	(
-		echo -e "Downloading patch...\n"
-		wget https://download2287.mediafire.com/uzd4l18ou6hgtVelarOGjJPKZg_QxAPUVMoZiZp-QcolWub8GdRC0YqtpBP4pcCJMp-kee8IUfLKf20sdhBWMNRF75yiq8z5TXMYn20bmbmUUcetRyFPzh3RLYhPOpLXE8FdtpLCCIx-yv_oMYyVXzfodH5M6eOMxoc42Snu9bEm/2ztqob93nrug8yg/ANIMELEE+-+COMPLETE+EDITION+-+PATCH+1.0.zip
-		
-		echo -e "Extracting...\n"
-		unzip -o ANIMELEE+-+COMPLETE+EDITION+-+PATCH+1.0.zip
-		cd ANIMELEE\ -\ COMPLETE\ EDITION/
-							
-		echo -e "Patching...\n"
-		xdelta3 -d -f -s $HOME/Emulation/roms/gamecube/ssbm.iso "ANIMELEE - COMPLETE EDITION.xdelta" $HOME/Emulation/roms/gamecube/animelee.iso
-
-		echo -e "Removing unneccesary files...\n"
-		cd $HOME/Applications
-		rm ANIMELEE+-+COMPLETE+EDITION+-+PATCH+1.0.zip
-		rm -rf ANIMELEE\ -\ COMPLETE\ EDITION/
-		) | progress_bar "Patching, please wait..."
-		info "SSBM has been patched!"
-	fi
-}
-
 
 overclock () {
 	rm -rf gcadapter-oc-kmod
@@ -156,6 +157,32 @@ undo_overclock() {
 	make clean
 	sudo rm /usr/lib/modules/$(uname -r)/extra/gcadapter_oc.ko
 	sudo rm /etc/modules-load.d/gcadapter_oc.conf
+}
+
+# unused functions
+patch_iso() {
+	# this is just temporary code for now and isn't currently used in the script; Mediafire is sketchy
+	if ! [ -f $HOME/Emulation/roms/gamecube/ssbm.iso ]; then
+		error "SSBM ISO not found. Please place it in $HOME/Emulation/roms/gamecube/ and name it to ssbm.iso"
+	else
+	(
+		echo -e "Downloading patch...\n"
+		wget https://download2287.mediafire.com/uzd4l18ou6hgtVelarOGjJPKZg_QxAPUVMoZiZp-QcolWub8GdRC0YqtpBP4pcCJMp-kee8IUfLKf20sdhBWMNRF75yiq8z5TXMYn20bmbmUUcetRyFPzh3RLYhPOpLXE8FdtpLCCIx-yv_oMYyVXzfodH5M6eOMxoc42Snu9bEm/2ztqob93nrug8yg/ANIMELEE+-+COMPLETE+EDITION+-+PATCH+1.0.zip
+		
+		echo -e "Extracting...\n"
+		unzip -o ANIMELEE+-+COMPLETE+EDITION+-+PATCH+1.0.zip
+		cd ANIMELEE\ -\ COMPLETE\ EDITION/
+							
+		echo -e "Patching...\n"
+		xdelta3 -d -f -s $HOME/Emulation/roms/gamecube/ssbm.iso "ANIMELEE - COMPLETE EDITION.xdelta" $HOME/Emulation/roms/gamecube/animelee.iso
+
+		echo -e "Removing unneccesary files...\n"
+		cd $HOME/Applications
+		rm ANIMELEE+-+COMPLETE+EDITION+-+PATCH+1.0.zip
+		rm -rf ANIMELEE\ -\ COMPLETE\ EDITION/
+		) | progress_bar "Patching, please wait..."
+		info "SSBM has been patched!"
+	fi
 }
 
 # Check if GitHub is reachable
@@ -282,6 +309,153 @@ Choice=$(main_menu)
 				done
 			fi
 		done
+	
+	elif [ "$Choice" == "Project+" ]; then
+		
+		while true; do
+		Choice=$(projectplus_menu)
+		
+			if [ $? -eq 1 ] || [ "$Choice" == "Exit" ]; then
+				break
+					
+			elif [ "$Choice" == "Download" ]; then
+				mkdir -p ProjectPlus
+				cd ProjectPlus
+				
+				# remove files if they have been previously downloaded
+				rm -rf *
+				
+				(
+				echo "33"
+				echo "# Downloading files..."
+				curl -L https://api.github.com/repos/jlambert360/FPM-AppImage/releases/latest | grep "browser_download_url" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+				
+				echo "66"
+				echo "# Extracting..."
+				tar -xf Launcher.tar.gz
+				tar -xf ProjectPlusSD*.tar.gz
+				
+				chmod +x Faster_Project_Plus-x86-64.AppImage
+				
+				echo "90"
+				echo "# Cleaning up..."
+				rm Launcher.tar.gz ProjectPlusSD*.tar.gz
+				) | progress_bar ""
+				
+				info "Project+ downloaded/updated!"
+				
+				cd $HOME/Applications
+			
+			elif [ "$Choice" == "Configure" ]; then
+				if ! [ -f ProjectPlus/Faster_Project_Plus-x86-64.AppImage ]; then
+					error "Project+ AppImage not found."
+				else
+					ProjectPlus/./Faster_Project_Plus-x86-64.AppImage
+				fi
+
+			elif [ "$Choice" == "SD" ]; then
+				settings_file=$HOME/.config/FasterPPlus/Dolphin.ini
+				
+				if ! [ -f $settings_file ]; then
+					error "Config file not found, please run Project+ prior to running this command."
+				else
+					# set the SD card path
+					sed -i "s#${HOME}/.local/share/FasterPPlus/Wii/sd.raw#${HOME}/Applications/ProjectPlus/sd.raw#g" $settings_file
+					
+					# change ISO paths from 0 to 1
+					sed -i 's#ISOPaths = 0#ISOPaths = 1#g' $settings_file
+					
+					# add launcher path
+					sed -i "9a\ISOPath0 = ${HOME}/Applications/ProjectPlus/Launcher" $settings_file
+						
+					# specify default ISO
+					sed -i "s#DefaultISO = #DefaultISO = ${HOME}/Emulation/roms/wii/ssbb.iso#g" $settings_file
+					
+					info "File paths added!"
+				fi
+			
+			elif [ "$Choice" == "Netplay" ]; then
+				if ! [ -f ProjectPlus/Faster_Project_Plus-x86-64.AppImage ]; then
+					error "Project+ AppImage not found."
+				else
+					ProjectPlus/./Faster_Project_Plus-x86-64.AppImage "$HOME/Applications/ProjectPlus/Launcher/Project+ Netplay Launcher.dol"
+				fi
+			
+			elif [ "$Choice" == "Offline" ]; then
+				if ! [ -f ProjectPlus/Faster_Project_Plus-x86-64.AppImage ]; then
+					error "Project+ AppImage not found."
+				else
+					ProjectPlus/./Faster_Project_Plus-x86-64.AppImage "$HOME/Applications/ProjectPlus/Launcher/Project+ Offline Launcher.dol"
+				fi
+			
+			elif [ "$Choice" == "Changelog" ]; then
+				xdg-open https://projectplusgame.com/changes
+					
+			fi
+		done
+	
+	elif [ "$Choice" == "PMEX" ]; then
+		
+		while true; do
+		Choice=$(pmex_menu)
+		
+			if [ $? -eq 1 ] || [ "$Choice" == "Exit" ]; then
+				break
+					
+			elif [ "$Choice" == "Download" ]; then
+				mkdir -p PMEX-Remix
+				cd PMEX-Remix
+				
+				info "Note: due to PMEX Remix being hosted on Google Drive, you'll need to manually download the Dolphin release.\n
+				Click OK to open the Google Drive link in your web browser. Save the .rar file to $HOME/Applications/PMEX-Remix.\n
+				Press Enter in the terminal after the download is complete to finish the setup."
+				xdg-open https://drive.google.com/drive/folders/1u6aENdnSyDio-CmpNRLg8NXxc8xoYfQh
+				read -p "Press Enter after downloading to extract the archive: " </dev/tty
+				
+				echo -e "Extracting...\n"
+				unrar x -o+ PMEX\ REMIX\ 0.95DX\ \[DOLPHIN\].rar
+				
+				echo -e "Copying textures...\n"
+				cp -r PMEX\ REMIX\ 0.95DX\ \[DOLPHIN\]/User/Load/Textures/RSBE01/ ~/.local/share/FasterPPlus/Load/Textures/
+				
+				echo -e "Cleaning up...\n"
+				rm PMEX\ REMIX\ 0.95DX\ \[DOLPHIN\].rar
+				
+				info "PMEX Remix downloaded!"
+				info "Please remember to do the following:\n
+				1. Configure your launcher path, your default ISO, and your SD card path in P+.\n
+				2. Set P+ in fullscreen, set aspect ratio to 16:10 if you're on Deck.\n
+				3. Check 'Load custom textures' from Graphics -> Advanced.\n
+				4. Set up your controls in Game Mode if you're not using a GCC."
+				
+				cd $HOME/Applications
+			
+			elif [ "$Choice" == "Configure" ]; then
+				if ! [ -f ProjectPlus/Faster_Project_Plus-x86-64.AppImage ]; then
+					error "Project+ AppImage not found."
+				else
+					ProjectPlus/./Faster_Project_Plus-x86-64.AppImage
+				fi
+	
+			elif [ "$Choice" == "Netplay" ]; then
+				if ! [ -f ProjectPlus/Faster_Project_Plus-x86-64.AppImage ]; then
+					error "Project+ AppImage not found."
+				else
+					ProjectPlus/./Faster_Project_Plus-x86-64.AppImage "$HOME/Applications/PMEX-Remix/PMEX REMIX 0.95DX [DOLPHIN]/Launcher/P+EX REMIX NETPLAY/Dolphin_Remix_Netplay_Launcher.elf"
+				fi
+			
+			elif [ "$Choice" == "Offline" ]; then
+				if ! [ -f ProjectPlus/Faster_Project_Plus-x86-64.AppImage ]; then
+					error "Project+ AppImage not found."
+				else
+					ProjectPlus/./Faster_Project_Plus-x86-64.AppImage "$HOME/Applications/PMEX-Remix/PMEX REMIX 0.95DX [DOLPHIN]/Launcher/P+EX REMIX OFFLINE/Project_Offline_Launcher.elf"
+				fi
+			
+			elif [ "$Choice" == "Guide" ]; then
+				xdg-open https://linuxgamingcentral.com/posts/how-to-setup-project-m-ex-on-deck/
+					
+			fi
+		done	
 	
 	elif [ "$Choice" == "Overclock" ]; then
 		
